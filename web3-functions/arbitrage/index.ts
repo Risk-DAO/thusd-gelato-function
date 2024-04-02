74fbfd3cebafde639111d15077169924eeedcea6
Alfu <Alfulinku@gmail.com>
2024-04-02 17:39:07 +0200

    fix btc decimal number

--- web3-functions/arbitrage/index.ts
+++ web3-functions/arbitrage/index.ts
@@ -8,9 +8,9 @@ const configs = [
         arbContractAddress: "0x176F6373d69274Bc6420edBd6050eCC9430CFC00",
         profitReceiver: "0x7095F0B91A1010c11820B4E263927835A4CF52c9",
         bamm: "0x920623AcBa785ED9a70d33ACab53631e1e834675",
-        amounts: [BigNumber.from(1).mul(BigNumber.from(10).pow(17)), // 0.1 tbtc
-        BigNumber.from(1).mul(BigNumber.from(10).pow(18)), // 1 tbtc
-        BigNumber.from(10).mul(BigNumber.from(10).pow(18))], // 10 btc
+        amounts: [BigNumber.from(1).mul(BigNumber.from(10).pow(7)), // 0.1 WBTC
+        BigNumber.from(1).mul(BigNumber.from(10).pow(8)), // 1 WBTC
+        BigNumber.from(10).mul(BigNumber.from(10).pow(8))], // 10 WBTC
         abi: [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [{ "internalType": "address", "name": "bamm", "type": "address" }], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "btcQty", "type": "uint256" }, { "internalType": "address", "name": "bamm", "type": "address" }, { "internalType": "address", "name": "profitReceiver", "type": "address" }], "name": "swap", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "int256", "name": "amount0Delta", "type": "int256" }, { "internalType": "int256", "name": "amount1Delta", "type": "int256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "uniswapV3SwapCallback", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "stateMutability": "payable", "type": "receive" }]
     },
     {
